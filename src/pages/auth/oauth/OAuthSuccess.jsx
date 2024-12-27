ebbc9347fc50705ce0d277849e393fa77d9f915c
akgarg0472 <akgarg0472@gmail.com>
2024-12-27 14:33:52 +0530

    printing oauth callback params

--- src/pages/auth/oauth/OAuthSuccess.jsx
+++ src/pages/auth/oauth/OAuthSuccess.jsx
@@ -5,6 +5,8 @@ const OAuthSuccess = () => {
   const [searchParams] = useSearchParams();
   const queryParams = Object.fromEntries(searchParams.entries());
 
+  console.log("OAuth response:", JSON.stringify(queryParams));
+
   if (window.opener) {
     window.opener.postMessage(
       { type: "oauth2_auth_success_response", params: queryParams },
